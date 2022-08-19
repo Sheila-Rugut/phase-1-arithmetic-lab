f60e9cab1a291ad30c7dd8a43287c34d261fc48e
Sheila <rugut.cheruto@student.moringaschool.com>
2022-08-19 16:13:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
 let num1=31;
 let num2=2;
 const multiply=num1*num2;
-let random = 0;
-random++;
-random;
+const random=Math.floor(Math.random() * 10) + 1;
 let num3=64;
 let num4=10;
 const mod=num3%num4;
