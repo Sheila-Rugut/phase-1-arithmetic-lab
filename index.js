455c4e2951f9c2210dc261b40cbfc060ebadbc3f
Sheila <rugut.cheruto@student.moringaschool.com>
2022-08-19 09:01:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+const multiply=num1*num2;
+let random = 0;
+random++;
+random;
+let num3=64;
+let num4=10;
+const mod=num3%num4;
+let max=Math.max(5, 8, 10, 20);
